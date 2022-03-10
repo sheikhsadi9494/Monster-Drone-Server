0f6fdb6a6eb091bcec375f59290d536c656afca6
sheikhsadi9494 <sadis6343@gmail.com>
2022-03-11 01:07:55 +0600

    commited by programmer sheikh sadi

--- index.js
+++ index.js
@@ -20,7 +20,15 @@ async function run() {
         const database = client.db("monster-drone");
         const productsCollection = database.collection('products');
         const ordersCollection = database.collection('orders');
-    
+        const reviewCollection = database.collection('reviews');
+        const usersCollectoin = database.collection('users');
+
+         // post product data
+         app.post('/products', async (req, res) => {
+            const product = req.body;
+            const reuslt = await productsCollection.insertOne(product);
+            res.json(reuslt);
+        })
         //get all products data
         app.get('/products', async (req, res) => {
             const query = productsCollection.find({});
@@ -68,6 +76,53 @@ async function run() {
             const query = { _id: ObjectId(id)};
             const result = await ordersCollection.deleteOne(query);
             res.json(result);
+        })
+        // post review data
+        app.post('/reviews', async (req, res) => {
+            const review = req.body;
+            const result = await reviewCollection.insertOne(review);
+            res.json(result);
+        })
+        // get all review data 
+        app.get('/reviews', async (req, res) => {
+            const cursor = reviewCollection.find({});
+            const result = await cursor.toArray();
+            res.json(result)
+        })
+        // post users data
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollectoin.insertOne(user);
+            res.json(result);
+        })
+        // usert for cheack uesr 
+        app.put('/users', async (req, res) => {
+            const user = req.body;
+            const filter = {email: user.email};
+            const options = { upsert: true };
+            const updateDoc = {$set: user};
+            const reslut = await usersCollectoin.updateOne(filter, updateDoc, options);
+            res.json(reslut);
+        })
+        // update user as admim
+        app.put('/users/admin', async (req, res) => {
+            const user = req.body;
+            console.log('pur', user);
+            const filter = {email: user.email};
+            const updateDoc = { $set: {role: 'admin'} };
+            const result = await usersCollectoin.updateOne(filter, updateDoc);
+            res.json(result);
+        })
+        // cheak user admin or not
+        app.get('/users/:email', async (req, res) => {
+            const email = req.params.email;
+            const query = {email: email};
+            const user = await usersCollectoin.findOne(query);
+            let isAdmin = false;
+            if( user?.role === 'admin' ){
+                isAdmin = true;
+            }
+            res.json({admin: isAdmin});
         })
     }
     finally {
