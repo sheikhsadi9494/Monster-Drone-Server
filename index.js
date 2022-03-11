51d219cfb507f15c3600638c40069fcd15494b28
sheikhsadi9494 <sadis6343@gmail.com>
2022-03-11 10:42:39 +0600

    commited by admin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
+const fileUpload = require('express-fileupload');
 const ObjectId = require('mongodb').ObjectId;
 const app = express()
 const port = process.env.PORT || 5000;
@@ -9,6 +10,7 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+app.use(fileUpload());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4wgcq.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -26,9 +28,27 @@ async function run() {
          // post product data
          app.post('/products', async (req, res) => {
             const product = req.body;
-            const reuslt = await productsCollection.insertOne(product);
-            res.json(reuslt);
-        })
+            const result = await productsCollection.insertOne(product);
+            res.json(result);
+         })
+        //  app.post('/products', async (req, res) => {
+        //    const productName = req.body.productName;
+        //    const price = req.body.price;
+        //    const discription = req.body.discription;
+        //    const img = req.files.img;
+        //    const imgData = img.data;
+        //    const encodedPic = imgData.toString('base64');
+        //    const imgBuffer = Buffer.from(encodedPic, 'base64');
+        //    const product = {
+        //        productName,
+        //        price,
+        //        discription,
+        //        img: imgBuffer
+        //    }
+        //    const result = await productsCollection.insertOne(product);
+        //    res.json(result)
+        // })
+        
         //get all products data
         app.get('/products', async (req, res) => {
             const query = productsCollection.find({});
@@ -40,6 +60,13 @@ async function run() {
             const id = req.params.id;
             const query = { _id: ObjectId(id)};
             const result = await productsCollection.findOne(query);
+            res.json(result);
+        })
+        //delete product data
+        app.delete('/products/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id)};
+            const result = await productsCollection.deleteOne(query);
             res.json(result);
         })
         // post order data
@@ -57,12 +84,6 @@ async function run() {
             const orders = await cursor.toArray();
             res.json(orders)
         })
-        // get all orders data
-        // app.get('/orders', async (req, res) => {
-        //     const query = ordersCollection.find({});
-        //     const result = await query.toArray();
-        //     res.json(result);
-        // })
         // get all orders by thire id
         app.get('/orders/:id', async (req, res) => {
             const id = req.params.id;
@@ -76,6 +97,34 @@ async function run() {
             const query = { _id: ObjectId(id)};
             const result = await ordersCollection.deleteOne(query);
             res.json(result);
+        })
+        // get allorders data
+        app.get('/allOrders', async (req, res) => {
+            const query = ordersCollection.find({});
+            const result = await query.toArray();
+            res.json(result);
+        })
+        // get allorders data by thire id
+        app.get('/allOrders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id)};
+            const result = await ordersCollection.findOne(query);
+            res.json(result);
+        })
+        // delete allorders
+        app.delete('/allOrders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id)};
+            const result = await ordersCollection.deleteOne(query);
+            res.json(result);
+        })
+         // update status
+         app.put('/allOrders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id)};
+            const updateDoc = {$set: {status: 'approved'}};
+            const result = await ordersCollection.updateOne(query, updateDoc);
+            res.json(result)
         })
         // post review data
         app.post('/reviews', async (req, res) => {
